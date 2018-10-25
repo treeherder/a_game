fa677fadd1ccb4ac362ccefe948f523447360b73
Fiver <brendan@packetfire.org>
2018-10-24 21:33:34 -0500

    Testing the creation of some basic objects and combinations of objects.

--- test/stage/environment_test.clj
+++ test/stage/environment_test.clj
@@ -12,6 +12,24 @@
   (testing "Accepts a [stage] and a parses the lists of entities and
    plots them on the map to be rendered:
    [[12 23 45] {some entity} ]" )
+
   (is (true? false )))
 ;; right now, this doesn't make a lot of sense outside of being a test function?
+
+(defn test_blade
+  "This object is made of mostly low-quality metal, and sharpened into a thin blade."
+  []
+  (components.properties/create_entity
+	:entity (actors.agent/_entity)
+  	:components {:hp 1, :hardness 5, :used_for ["handle", "kindling"] :reducible ["iron" ["smelting", "filing"]] }))
+
+(defn scrap_wood
+  "This object is made at least partially of low-quality wood. It is potentially flammable."
+  [] ;;this could have optional keywords with default values for things like HP?
+  (components.properties/create_entity
+	:entity (actors.agent/_entity)
+	:components {:hp 1, :hardness 5, :used_for ["handle", "kindling"] :inflamable true}))
+
+
+(defn knife "A simple test object." [handle blade] 
 
