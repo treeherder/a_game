0b3436995201da47be00c9ea17ab8efa5f6a6ad3
Fiver <brendan@packetfire.org>
2018-10-24 21:32:13 -0500

    Create entities and attribute them  with components.

--- src/actors/agent.clj
+++ src/actors/agent.clj
@@ -8,12 +8,11 @@
 (defn _entity
   "A more effective, no-longer alternate case where everthing is a set, or a vector in a set."
   []
-  {(name_entity) :components })
+  {:id (name_entity) :components #{}})
 
 (defn agent_pool
   "The part of the stage object that holds all of the actors."
   [stage]                                            ;; stage container for  agents
   (let [stage (stage :agent_pool)]
-    ())
+    ()))
   ;; poll a database of agents, return a map by identity of all available agents on a stage
-  )
--- src/components/properties.clj
+++ src/components/properties.clj
@@ -1,18 +1,29 @@
 (ns components.properties
   (:gen-class))
 
-(defn add_component
-  "Gives the entity a oomponent value and puts in the data structure.
-  A perfect entity should be
-  {:id UUID :components [{:component1 zzz} {:component22 etc}]"
-  [entity component]
-  (let [entity (transient entity)]
-    (persistent! (assoc! entity :components component)))
+(defn create_entity
+  "Gives the entity a component value and puts in the data structure."
+  [& {:keys [entity, components]}]
+  (let[ entity (transient entity)
+        entity_id (entity :id)
+ 	entity_components (entity :components)
+ 	new_components (conj entity_components components)]
+    (assoc! entity :components new_components)
+    (persistent! entity)))
+	 
+
+(defn get_properties
+  "Get properties of a given entity."
+  [entity]
+  (let[ entity (transient entity)
+        entity_id (entity :id)
+        entity_components (entity :components)]
+    (prn entity_components))
   )
-
-
 
 (defn set_component_configuration
   "Configure an entity's components according to component change parameters"
   [entity component_changes]
-  )
+  
+  (prn "ok")
+)
