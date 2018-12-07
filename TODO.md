b82b4a2647de28775350c6ade9d789232717fbcd
Fiver <brendan@packetfire.org>
2018-12-06 21:22:09 -0600

    Worked on outling expectations for tests about stage elements and began to organize engine.
    Signed-off-by: Fiver <brendan@packetfire.org>

--- TODO.md
+++ TODO.md
@@ -9,3 +9,13 @@
   3. Re-Document codebase
   4. ????
   5. Profit. 
+
+
+
+
+##### Code:
+1. A single tile in a stage:
+`{:coordinates (XYZ), :kind TILE_SET_TYPE  :exists bool, :passable bool, :destructable bool :contains TILE_CONTENTS_TYPE :occupied/by bool/list }`
+   1. After ascertaining that the tile exists,  the engine must reference the type, contents, and occupants before attempting to render it.
+     1. The tile type, contents, and occupants must all be collections of the appropriate entites, which in turn must all obey the rules set in the stage.  The game engine would thus be responsible for building the individual elements and negotiating their inter-relationships.
+   
--- src/stage/environment.clj
+++ src/stage/environment.clj
@@ -5,18 +5,16 @@
 ;; create a stage map and store geographical information
 ;; use premade map?  empty grid?  random map?
 ;; if using a premade map, needs parsing and storage?
+
 (defn create_stage
   "Name a stage and define its map."
   ;;stage should have keyword arguments to produce overloaded type
-  [label agent_pool item_pool mapfile coordgrids rules&]
-  (let [label {:label label}
-        ]
-    ()
-    )
-  )
+  [label agent_pool item_pool mapfile coordgrids rules &]
+  ())
 
-(defn build_tree "An ambiguous place where everything lives." [worldmap entity]
-  (swap! worldmap conj  entity))
+(defn build_tree "An ambiguous place where everything lives."
+      [worldmap entity]
+      (swap! worldmap conj  entity))
 ;; Not sure how to do this without atoms, but this makes enough sense for now.
 
 ;; a Stage needs to be a structure that somehow contains all of the ambiguous and explicit data about a game
